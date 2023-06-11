9d11524b2f7455e308e9b2462cc229474f6cd77c
PANFACTORY <yutatanabe510@gmail.com>
2023-06-11 12:01:30 +0900

    :sparkle: Add ls-files and hash-object command

--- src/main.go
+++ src/main.go
@@ -13,5 +13,9 @@ func main() {
 	switch os.Args[1] {
 	case "cat-file":
 		ExecuteCatFile(os.Args[2])
+	case "hash-object":
+		ExecuteHashObject(os.Args[2])
+	case "ls-files":
+		ExecuteLsFiles()
 	}
 }
